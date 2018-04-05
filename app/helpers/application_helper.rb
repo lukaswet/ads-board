583b3c8637ebbf373335ee808b7ea8d5e53b5a51
lukaswet <lukaswet@gmail.com>
2018-04-05 12:54:24 +0300

    Repairing callbacks 4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def gravatar_url(user)
-    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
     if user.image
       "http://graph.facebook.com/#{user.uid}/picture?type=large"
     else
+      gravatar_id = Digest::MD5::hexdigest(user.email).downcase
       "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
     end
 
